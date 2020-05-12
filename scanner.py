da2f3cfa62933c58f3d41cea8a739c35636d5e74
Laykel <layke.luc@gmail.com>
2020-05-12 09:54:02 +0200

    Setup commit

---
+++ scanner.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+Lab: 4-KneeScan
+Authors: Claude-André Alves, Luc Wachter
+Description: Provide four visualizations of a knee scan.
+Date: 12.05.2020
+Python version: 3.7.4
+"""
+
+import vtk
+
+
+# Main instructions
+def main():
+    print("Yeeeet")
+
+
+if __name__ == "__main__":
+    main()
