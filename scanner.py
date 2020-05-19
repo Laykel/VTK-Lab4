c572f84806027a2b92a2562c5640fd2a03420193
Laykel <layke.luc@gmail.com>
2020-05-19 12:02:36 +0200

    Position knees and rotate

--- scanner.py
+++ scanner.py
@@ -10,7 +10,12 @@ Python version: 3.7.4
 https://lorensen.github.io/VTKExamples/site/Python/IO/ReadSLC/
 """
 
+from time import sleep
 import vtk
+
+
+# The time a frame stays on screen (seconds)
+FRAMERATE = 0.01
 
 
 def read_slc_file(filename):
@@ -46,8 +51,15 @@ def main():
     ren_win.SetWindowName("The good knee")
     ren_win.SetSize(1000, 1000)
 
-    iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(ren_win)
+    # Create an actor from the SLC file
+    knee = read_slc_file("vw_knee.slc")
+
+    transform = vtk.vtkTransform()
+    transform.PostMultiply()
+    knee.SetUserTransform(transform)
+
+    # Rotate the knee in its initial position
+    transform.RotateX(90)
 
     # Define viewport ranges
     xmins = [0, .5, 0, .5]
@@ -55,25 +67,30 @@ def main():
     ymins = [0, 0, .5, .5]
     ymaxs = [0.5, 0.5, 1, 1]
 
-    knee = read_slc_file("vw_knee.slc")
+    renderers = [vtk.vtkRenderer(), vtk.vtkRenderer(), vtk.vtkRenderer(), vtk.vtkRenderer()]
 
+    # Create the viewports and generate the visualizations
     for i in range(4):
-        ren = vtk.vtkRenderer()
+        ren = renderers[i]
         ren_win.AddRenderer(ren)
-
-        if i == 0:
-            camera = ren.GetActiveCamera()
-        else:
-            ren.SetActiveCamera(camera)
 
         ren.SetViewport(xmins[i], ymins[i], xmaxs[i], ymaxs[i])
 
         # Create a mapper and actor
         ren.AddActor(knee)
+        ren.SetBackground(0.5, 0.5, 0.5)
+        ren.GetActiveCamera().Elevation(180)
         ren.ResetCamera()
 
-    ren_win.Render()
-    iren.Start()
+    # Rotate all objects to have a 360 view
+    for _ in range(360):
+        for ren in renderers:
+            ren.GetActiveCamera().Azimuth(1)
+
+        ren_win.Render()
+        sleep(FRAMERATE)
+
+    sleep(2)
 
 
 if __name__ == "__main__":
