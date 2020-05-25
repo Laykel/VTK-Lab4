331a46072e7c35e9226241d3f7316ee857c6deea
Laykel <layke.luc@gmail.com>
2020-05-25 17:23:08 +0200

    Added outline and refactored

--- scanner.py
+++ scanner.py
@@ -13,32 +13,52 @@ https://lorensen.github.io/VTKExamples/site/Python/IO/ReadSLC/
 from time import sleep
 import vtk
 
-
 # The time a frame stays on screen (seconds)
 FRAMERATE = 0.01
+SLC_FILENAME = "vw_knee.slc"
 
 
-def read_slc_file(filename):
-    reader = vtk.vtkSLCReader()
-    reader.SetFileName(filename)
-    reader.Update()
-
-    # Create a mapper.
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(reader.GetOutputPort())
-
-    # Implementing Marching Cubes Algorithm to create the surface using vtkContourFilter object.
-    contour_filter = vtk.vtkContourFilter()
-    contour_filter.SetInputConnection(reader.GetOutputPort())
-    contour_filter.SetValue(0, 72.0)
-
+def outline(vtk_object):
+    """Create an actor representing an outline for the object
+    :param vtk_object: The vtk object around which the outline must appear
+    :returns: A vtkActor representing an outline around the object
+    """
     outliner = vtk.vtkOutlineFilter()
-    outliner.SetInputConnection(reader.GetOutputPort())
+    outliner.SetInputConnection(vtk_object.GetOutputPort())
     outliner.Update()
 
+    outline_mapper = vtk.vtkPolyDataMapper()
+    outline_mapper.SetInputConnection(outliner.GetOutputPort())
+
+    outline_actor = vtk.vtkActor()
+    outline_actor.SetMapper(outline_mapper)
+    outline_actor.GetProperty().SetColor(0, 0, 0)
+    return outline_actor
+
+
+def contour(vtk_object, i, j):
+    """...
+    :param vtk_object: The object to apply contouring to
+    :param i: ...
+    :param j: ...
+    :returns: A vtkContourFilter to add to the pipeline
+    """
+    # Implementing Marching Cubes Algorithm to create the surface using vtkContourFilter object.
+    contourer = vtk.vtkContourFilter()
+    contourer.SetInputConnection(vtk_object.GetOutputPort())
+    contourer.SetValue(i, j)
+
+    return contourer
+
+
+def create_actor(vtk_object):
+    """Create an actor from a vtkObject
+    :param vtk_object: The object for which to create an actor
+    :returns: An actor representing the object sent as parameter
+    """
     mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(contour_filter.GetOutputPort())
-    mapper.SetScalarVisibility(0)
+    mapper.SetInputConnection(vtk_object.GetOutputPort())
+    mapper.SetScalarVisibility(False)
 
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
@@ -51,12 +71,22 @@ def main():
     ren_win.SetWindowName("The good knee")
     ren_win.SetSize(1000, 1000)
 
-    # Create an actor from the SLC file
-    knee = read_slc_file("vw_knee.slc")
+    # Create an SLC reader
+    reader = vtk.vtkSLCReader()
+    reader.SetFileName(SLC_FILENAME)
+    reader.Update()
 
+    # Create actors
+    knee_outline = outline(reader)
+    knee_bone = create_actor(contour(reader, 0, 72.0))
+    knee_skin = create_actor(contour(reader, 0, 50))
+
+    # Create a transform object for the knee and its outline
     transform = vtk.vtkTransform()
     transform.PostMultiply()
-    knee.SetUserTransform(transform)
+    knee_bone.SetUserTransform(transform)
+    knee_outline.SetUserTransform(transform)
+    knee_skin.SetUserTransform(transform)
 
     # Rotate the knee in its initial position
     transform.RotateX(90)
@@ -67,19 +97,23 @@ def main():
     ymins = [0, 0, .5, .5]
     ymaxs = [0.5, 0.5, 1, 1]
 
-    renderers = [vtk.vtkRenderer(), vtk.vtkRenderer(), vtk.vtkRenderer(), vtk.vtkRenderer()]
+    # Renderers for the four viewports
+    renderers = [vtk.vtkRenderer() for _ in range(4)]
+    # Actors for the four viewports
+    actors = [knee_skin, knee_skin, knee_bone, knee_bone]
+    # Background colors for the four viewports
     colors = [(0.82, 0.82, 1), (0.82, 0.82, 0.82), (1, 0.82, 0.82), (0.82, 1, 0.82)]
 
     # Create the viewports and generate the visualizations
-    for i in range(4):
-        ren = renderers[i]
+    for idx, ren in enumerate(renderers):
         ren_win.AddRenderer(ren)
 
-        ren.SetViewport(xmins[i], ymins[i], xmaxs[i], ymaxs[i])
+        ren.SetViewport(xmins[idx], ymins[idx], xmaxs[idx], ymaxs[idx])
 
         # Create a mapper and actor
-        ren.AddActor(knee)
-        ren.SetBackground(colors[i])
+        ren.AddActor(actors[idx])
+        ren.AddActor(knee_outline)
+        ren.SetBackground(colors[idx])
         ren.GetActiveCamera().Elevation(180)
         ren.ResetCamera()
 
